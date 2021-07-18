1c21ad77670e6e5d91eeddfcf9eea6db83026982
hasika-sandaruwan <eduexpressinfo@gmail.com>
2021-07-18 13:32:55 +0530

    2

--- src/StudentFormController.java
+++ src/StudentFormController.java
@@ -1,2 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class StudentFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public Button btnSaveStudentOnAction;
+    public Button btnUpdateStudentOnAction;
+    public Button btnDeleteStudentOnAction;
+    public TextField txtSearch;
+    public TableView tblStudents;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+
+    public void btnSaveStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteStudentOnAction(ActionEvent actionEvent) {
+    }
 }
