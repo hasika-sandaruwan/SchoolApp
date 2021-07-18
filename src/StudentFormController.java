4e93bd0d2695496778c3d9ba1ce9354bbe978c5a
hasika-sandaruwan <eduexpressinfo@gmail.com>
2021-07-18 14:49:20 +0530

    7

--- src/StudentFormController.java
+++ src/StudentFormController.java
@@ -1,14 +1,19 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class StudentFormController {
     public TextField txtId;
@@ -22,8 +27,42 @@ public class StudentFormController {
     public TableColumn colContact;
     public TableColumn colAddress;
 
+
+    public void initialize() {
+
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+
+        loadAllStudents();
+    }
+
+    private void loadAllStudents() {
+        Configuration configuration =
+                new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Student.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Query query = session.createQuery("FROM Student");/*HQL(hibernate query language)*/
+        List<Student> list = query.list();
+        ObservableList<StudentTM> observableList = FXCollections.observableArrayList();
+
+        for (Student student : list
+        ) {
+            observableList.add(new StudentTM(
+                    student.getId(),student.getName(),student.getContact(),student.getAddress()
+            ));
+        }
+
+        tblStudents.setItems(observableList);
+
+    }
+
+
     public void btnSaveStudentOnAction(ActionEvent actionEvent) {
-        Student s1= new Student(
+        Student s1 = new Student(
                 txtId.getText(),
                 txtName.getText(),
                 txtContact.getText(),
