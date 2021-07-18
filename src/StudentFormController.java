d1621b2c3a51b3d0e0acaa6f876b96fe4dca5ba6
hasika-sandaruwan <eduexpressinfo@gmail.com>
2021-07-18 14:01:51 +0530

    4

--- src/StudentFormController.java
+++ src/StudentFormController.java
@@ -3,6 +3,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.io.Serializable;
 
 public class StudentFormController {
     public TextField txtId;
@@ -17,6 +23,25 @@ public class StudentFormController {
     public TableColumn colAddress;
 
     public void btnSaveStudentOnAction(ActionEvent actionEvent) {
+        Student s1= new Student(
+                txtId.getText(),
+                txtName.getText(),
+                txtContact.getText(),
+                txtAddress.getText()
+        );
+
+        Configuration configuration =
+                new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Student.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Serializable save = session.save(s1);
+        System.out.println(save);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
 
     }
 
